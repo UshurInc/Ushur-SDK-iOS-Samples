f18b7acc56a7260137f68433632bdb403f318e43
Kaushal-Ushur <kaushal.hebbar@ushur.com>
2024-04-04 10:17:51 +0530

    Updated SDK

--- UshurSDKDemo/ushur_sdk_demo/ViewController.swift
+++ UshurSDKDemo/ushur_sdk_demo/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
         imagePicker.delegate = self
         return imagePicker
     }()
-    var ushur = Ushur(token: "")
+    var ushur = Ushur(token: "",useEdgeOcr: true)
     //MARK: Outlets
     @IBOutlet weak var addImageButton: UIButton!
     @IBOutlet weak var uploadImageButton: UIButton!
